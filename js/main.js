32df5d3e79c7b5a184e490624cf75b9e5d251b04
Arthur Puyou <arthur@puyou.fr>
2014-04-26 19:14:18 +0200

    next steps

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function Matchs($scope, $http, $timeout) {
+  $scope.deleted = [];
+  
+  // Read with polling
+  (function poll(){
+    if(!$scope.retry){
+      $scope.retry = 3000;
+    }
+    $http.get('http://10.10.113.6/poolzorlive/ajax.php?page=pad_liste_matchs_finis').success(function(data) {
+      $scope.error = false;
+      $scope.matchs = data.matchs;
+      $scope.message = data.message || "";
+      $timeout(poll, $scope.retry);
+    }).error(function(data, status){
+      $scope.error = "Impossible de récupérer l'état";
+      console.error("Cannot get status:", data);
+      $scope.retry *= 2;
+      $timeout(poll, $scope.retry);
+    });
+  })();
+  
+  $scope.delete = function(id) {
+    $scope.deleted.push(id);
+  }
+}
